2fbe115492fdadad1cab1246aad2f79fe031fad0
Anaelica <anaanaelica@gmail.com>
2026-09-02 21:53:54 -0300

    Oreganização de todo Layout

--- web/components/feed/sections/feed-licitacoes-section.tsx
+++ web/components/feed/sections/feed-licitacoes-section.tsx
@@ -1,8 +1,9 @@
+import { NavUser } from "../components/nav-user";
+
 export function FeedLicitacoesSection() {
   return (
     <section className="space-y-6">
-      <h1 className="text-2xl font-semibold">Licitações</h1>
-      {/* licitacoes content goes here */}
+      <NavUser />
     </section>
   );
-}
+} 
